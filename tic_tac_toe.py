4768dfa2f1e64e977849e8f7ee6f6b947dd83530
oshi <shkirya.2004@mail.ru>
2024-11-27 03:23:42 +0300

    Edited code tic-tac-toe.py: del global's, create new way clean field

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,36 +3,22 @@ field = [['-', '-', '-'],
          ['-', '-', '-']]
 flag_move = True # Маркер для определения череды хода
 
-
-def clean_field(): # Очистка игрового поля
-    global field
-    global flag_move
-    field = [['-', '-', '-'],
-             ['-', '-', '-'],
-             ['-', '-', '-']]
-    flag_move = True
-    show_field(field)
-
 def check_winner(fields:list): # Проверка на завершенность игры
     for i in fields: # Проверка победы по строкам
         if i[0] == i[1] == i[2] and i[0] != '-':
             print(f'Победили {i[0]}')
-            clean_field()
             return
     for i in range(3): # Проверка победы по столбцам
         if fields[0][i] == fields[1][i] == fields[2][i] and fields[0][i] != '-':
             print(f'Победили {fields[0][i]}')
-            clean_field()
             return
     if ((fields[0][0] == fields[1][1] == fields[2][2]) or (fields[2][0] == fields[1][1] == fields[0][2])) and fields[1][1] != '-': # Проверка победы по диагоналям
         print(f'Победили {fields[1][1]}')
-        clean_field()
         return
     for i in fields: # Проверка на оконченность игры
         if '-' in i:
-            return
+            return -1 # Признак того, что игра не закончилась
     print('Ничья.')
-    clean_field()
     return
 
 def show_field(fields: list): # Отображение поля в консоль
@@ -61,4 +47,9 @@ while True:
         field[move[0]][move[1]] = 'x' if flag_move else 'o'
         flag_move = not flag_move
         show_field(field)
-        check_winner(field)
+        if check_winner(field) != -1: # Проверка признака оконченности игры, очистка поля и установка хода для "X"
+            flag_move = True
+            field = [['-', '-', '-'],
+                     ['-', '-', '-'],
+                     ['-', '-', '-']]
+            show_field(field)
